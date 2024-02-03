85603b4d46e7c9f15014487dc2ef619352dab04f
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2024-02-03 19:57:29 -0300

    subindo resolucao exercicios

---
+++ src/Exercicio_04.java
@@ -0,0 +1,9 @@
+public class Exercicio_04 {
+    //Faça um programa que utilize um loop `for` para imprimir os
+    //números de 1 a 10.
+    public static void main(String[] args) {
+        for(int i = 1; i < 11; i++){
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/Exercicio_05.java
@@ -0,0 +1,12 @@
+public class Exercicio_05 {
+    //Escreva um programa que utilize o loop `foreach` para percorrer
+    //e exibir os elementos de um array.
+
+    public static void main(String[] args) {
+        String[] nomes = {"Ewerton","Bernardo","Benjamim","Enzo"};
+
+        for(String nome : nomes){
+            System.out.println("Seja bem-vindo " + nome);
+        }
+    }
+}
